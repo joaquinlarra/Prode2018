ef5cda0347c3ab2164ab392af7fd0ed453a19331
Joaquin Astelarra <joaquinastelarra@gmail.com>
2018-04-14 19:50:41 -0300

    Adjust highlight responsive

--- assets_fe/js/main.js
+++ assets_fe/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  if ($(window).width() < 768) {
-  } else {
+  if ($(window).width() > 768) {
     var tour = introJs()
     tour.setOptions({ doneLabel: 'Listo', showBullets: false }).start()
   }
