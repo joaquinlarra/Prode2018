fbee7c4586fe1c0625c4955724cc416087434923
Joaquin Astelarra <joaquinastelarra@gmail.com>
2018-05-01 17:59:18 -0300

    allow origin

--- application/views/front/common/header_mini.php
+++ application/views/front/common/header_mini.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <title><?=$page_title?></title>
+      <?php header('Access-Control-Allow-Origin: *'); ?>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?=$page_description?>">
